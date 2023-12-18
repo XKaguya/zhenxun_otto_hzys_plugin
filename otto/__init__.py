4ffe24567256b6c36563d7f08ebccedb00d5b93b
白泽 <rx700@vip.qq.com>
2023-12-19 00:29:31 +0800

    Add files via upload

---
+++ otto/__init__.py
@@ -0,0 +1,61 @@
+from nonebot import on_command
+from nonebot.adapters.onebot.v11.bot import Bot
+from pathlib import Path
+from nonebot import on_command
+from nonebot.adapters.onebot.v11.bot import Bot
+from pathlib import Path
+from utils.message_builder import image
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot.adapters.onebot.v11.event import Event
+from nonebot.log import logger
+from .utils import otto as OTTO
+from configs.config import Config
+from utils.message_builder import record
+
+__zx_plugin_name__ = "OTTO活字印刷术"
+__plugin_usage__ = """
+usage：
+/otto 阿米诺斯
+默认开启原声大碟模式，即如果有整句直接使用整句语音。可以自定义后缀False取消。
+""".strip()
+__plugin_des__ = "OTTO活字印刷术"
+__plugin_type__ = ("一些工具",)
+__plugin_cmd__ = ["OTTO活字印刷术"]
+
+__plugin_settings__ = {
+	"level": 5,
+	"default_status": True,
+	"limit_superuser": False,
+	"cmd": ["otto"],
+}
+
+Config.add_plugin_config("Otto", "Download_Path", None, help_="存放下载的音频文件的文件夹")
+
+
+Config.add_plugin_config("Otto", "Website", "https://otto-hzys.hanayabuki.cf", help_="从哪里活字印刷")
+
+otto = on_command("otto", priority=5, block=True)
+@otto.handle()
+async def _(bot: Bot, ev: Event):
+	download_path = Config.get_config("Otto", "Download_Path")
+	logger.info(download_path)
+
+	website = Config.get_config("Otto", "Website")
+	logger.info(website)
+
+	user_input_pre = ev.get_plaintext().strip('/')
+	user_input_after = user_input_pre.strip('otto')
+	user_input = user_input_after.strip()
+	logger.info(f"User input: {user_input}")
+
+	default_bool_value = True
+	
+	if 'false' in user_input or 'False' in user_input:
+		default_bool_value = False
+	else:
+		pass
+
+	file_path = await OTTO.call_otto(user_input, default_bool_value, download_path, website)
+	logger.info(f"File path: {file_path}")
+	file = record(file_path)
+	await otto.send(file)
---
+++ otto/utils/otto.py
@@ -0,0 +1,91 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+import time
+import os
+from nonebot.log import logger
+import shutil
+import asyncio
+import subprocess
+
+async def generate_otto(driver, text, original_disk_mode, path):
+	textarea = driver.find_element(By.XPATH, '//textarea[@class="el-textarea__inner"]')
+	textarea.clear()
+	textarea.send_keys(text)
+
+	if original_disk_mode:
+		switch_button = driver.find_element(By.CLASS_NAME, "el-switch__input")
+		driver.execute_script("arguments[0].click();", switch_button)
+
+	button = driver.find_element(By.XPATH, '//button/span[text()="生成otto鬼叫"]')
+	button.click()
+	
+	await asyncio.sleep(5)
+	
+	download_button = driver.find_element(By.XPATH, '//button[@class="el-button el-button--primary" and span[text()="下载原音频"]]')
+	download_button.click()
+	
+	driver.quit()
+	
+	files = await get_files_in_directory(path)
+	if files:
+		return files[0]
+	return None
+
+
+async def get_files_in_directory(path):
+	file_list = []
+	for root, dirs, files in os.walk(path):
+		for file in files:
+			file_list.append(os.path.join(root, file))
+	return file_list
+
+
+async def clear_directory(path):
+	for root, dirs, files in os.walk(path):
+		for file in files:
+			os.remove(os.path.join(root, file))
+		for dir in dirs:
+			shutil.rmtree(os.path.join(root, dir))
+
+
+async def call_otto(text, ysdd, path, website):
+	await clear_directory(path)
+	logger.info("Directory cleaned.")
+	
+	options = Options()
+	options.add_argument("--headless")
+
+	options.set_preference("browser.download.folderList", 2)
+	options.set_preference("browser.download.manager.showWhenStarting", False)
+	options.set_preference("browser.download.dir", path)
+	options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
+
+	driver = webdriver.Firefox(options=options)
+
+	driver.get(website)
+	
+	first_file = await generate_otto(driver, text, ysdd, path)
+	logger.info(f"Executed. File: {first_file}")
+	
+	mp3_file_path = await convert_and_send(first_file)
+	return mp3_file_path
+
+async def convert_to_mp3(input_file, output_file):
+	try:
+		subprocess.run(['ffmpeg', '-i', input_file, '-codec:a', 'libmp3lame', '-q:a', '2', output_file], check=True)
+		os.remove(input_file)
+		return output_file
+	except subprocess.CalledProcessError as e:
+		logger.error(f"FFmpeg error: {e}")
+		return None
+
+async def convert_and_send(file_path):
+	mp3_file_path = file_path.replace('.wav', '.mp3')
+	conversion_success = await convert_to_mp3(file_path, mp3_file_path)
+	
+	if conversion_success:
+		logger.info("Convert success.")
+	else:
+		logger.error("Conversion to MP3 failed.")
+	return mp3_file_path
